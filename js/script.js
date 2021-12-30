448235d7fdf84ad87654f59ec431f68a71199b00
Florencia <fdubininkas@gmail.com>
2021-12-29 23:11:15 -0300

    cambios en script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+let catuai = 1200;
+let chiapas = 1500;
+let kit = 2500;
+let producto1 = "";
+let producto2 = "";
+let precio = 0;
+let nombre = "";
+let iva = "";
+let seguirCompra = ""; 
+
 function saludar() {
     alert('Bienvenido/a ' + nombre + ', a continuacion podrá escoger productos de la tienda☕');
 }
@@ -8,14 +18,21 @@ function sumarCostos() {
             return catuai + chiapas;
         } else if (producto1 == "1" && producto2 == "3") {
             return catuai + kit;
-        } else if (producto1 == "2" && producto2 == "1") {
+        } 
+        else if (producto1 == "1" && producto2 =="1"){
+            return catuai + catuai;
+        }else if (producto1 == "2" && producto2 == "1") {
             return chiapas + catuai;
         }else if (producto1 == "2" && producto2 == "3"){
             return chiapas + kit;
+        }else if (producto1 == "2" && producto2 == "2"){
+            return chiapas + chiapas;
         }else if (producto1 == "3" && producto2 == "1"){
             return kit + catuai;
         }else if (producto1 == "3" && producto2 == "2"){
             return kit + chiapas;
+        }else if (producto1 == "3" && producto2 == "3"){
+            return kit + kit;
         }
     } else {
         if(producto1 == "1"){
@@ -41,19 +58,15 @@ function precioIva() {
         return alert("Tu precio final es $" + precio);
     }
 }
-// main
-let nombre = prompt("Ingrese su nombre");
+
+nombre = prompt("Ingrese su nombre");
 saludar();
 
-let catuai = 1200;
-let chiapas = 1500;
-let kit = 2500;
-let producto2 = "";
-let precio = 0;
-let producto1 = prompt(
+producto1 = prompt(
     "elija uno de nuestros productos \n 1: Catuaí Brasil \n 2: México Chiapas \n 3: Kit Cafetero"
 );
-let seguirCompra = prompt("¿Quiere seguir comprando?");
+
+seguirCompra = prompt("¿Quiere seguir comprando?");
 if (seguirCompra.toLowerCase() === 'si') {
     producto2 = prompt(
         "elija otro de nuestros productos \n 1: Catuaí Brasil \n 2: México Chiapas \n 3: Kit Cafetero"
@@ -61,5 +74,5 @@ if (seguirCompra.toLowerCase() === 'si') {
 }
 finalizarCompra();
 
-let iva = prompt('Ingrese su condición ante el IVA \n 1: Responsable Inscripto \n 2: Exenta');
+iva = prompt('Ingrese su condición ante el IVA \n 1: Responsable Inscripto \n 2: Exenta');
 precioIva();
