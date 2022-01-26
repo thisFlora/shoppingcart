aca36af50d87cfced84fca59cad03efb5eab8525
Florencia <fdubininkas@gmail.com>
2022-01-25 23:33:10 -0300

    eventos

--- js/script.js
+++ js/script.js
@@ -23,42 +23,99 @@ class Carrito {
 }
 
 function saludar() {
-    alert(
-        "Bienvenido/a " +
-        nombre +
-        ", a continuacion podrá escoger productos de la tienda☕"
-    );
+    (async () => {
+        const { value: nombre } = await Swal.fire({
+            title: "Bienvenido",
+            input: "text",
+            inputAttributes: {
+                autocomplete: "off",
+            },
+            inputLabel: "Ingrese su nombre",
+        });
+        if (nombre) {
+            Swal.fire(
+                `Hola ${nombre}, a continuación podrás escoger productos de la tienda☕ `
+            );
+        }
+    })();
 }
-nombre = prompt("Ingrese su nombre");
+
 saludar();
 
+//Si hay algo en el carrito, lo recupero.
 if (recuperarStorage("carrito")) {
     carrito = recuperarStorage("carrito");
 }
 
+//Capturo los objetos de los productos y cuando hagan clic ejecuto buscarId
+let catuai = document.getElementById("catuai");
+catuai.addEventListener("click", buscarId);
+
+let chiapas = document.getElementById("chiapas");
+chiapas.addEventListener("click", buscarId);
+
+let kit = document.getElementById("kit");
+kit.addEventListener("click", buscarId);
+
+//Guardo el valor del data-id del HTML y se lo paso a la funcion comprar
+function buscarId(e) {
+    e.preventDefault();
+    let btn = e.target;
+    let idBtn = parseInt(btn.getAttribute("data-id"));
+    comprar(idBtn);
+}
+
+//me fijo si hay un elemento con el mismo id, si hay le sumo 1 a la cantidad y si no, lo creo
 function comprar(id) {
     let idProd = id;
     if (carrito.some((element) => element.id === idProd)) {
         const found = carrito.find((element) => element.id === idProd);
         found.cantidad++;
+        mostrarAlert();
         guardarStorage("carrito", JSON.stringify(carrito));
         despliegaProductos();
     } else {
         const found = productos.find((element) => element.id === idProd);
-        const { id, producto, precio } = found;
+        const id = found.id;
+        const producto = found.producto;
+        const precio = found.precio;
         const agregarProd = new Carrito(id, producto, precio, 1);
         carrito.push(agregarProd);
         guardarStorage("carrito", JSON.stringify(carrito));
+        mostrarAlert();
         despliegaProductos();
     }
 }
 
-function despliegaProductos(){
+//muestro alerta de que se agregó el producto
+function mostrarAlert() {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "bottom-end",
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+    });
+    Toast.fire({
+        icon: "success",
+        title: "Se agregó el producto",
+    });
+}
+
+//Agrego los productos al carrito en el html
+function despliegaProductos() {
     const listaP = document.getElementById("lista");
     listaP.innerHTML = "";
     carrito.forEach((producto) => {
         let prod = document.createElement("li");
-        prod.innerHTML = `<p style="color: white">${producto.producto} - Precio: $${producto.precio} - Cantidad: ${producto.cantidad}</p>`;
+        prod.innerHTML = `<span style="color: white">${producto.producto}</span>  
+                            <span style="color:white">Precio: $${producto.precio}</span>  
+                            <span style="color:white">Cantidad: ${producto.cantidad}</span>
+                            `;
         listaP.appendChild(prod);
     });
 }
@@ -79,9 +136,11 @@ function sumarCostos() {
     return nPrecio;
 }
 
+let finaliza = document.getElementById("fin");
+finaliza.addEventListener("click", finalizarCompra);
+
 function finalizarCompra() {
     precioFinal = sumarCostos();
-    alert("El total de su compra es: $" + precioFinal);
     iva = prompt(
         "Ingrese su condición ante el IVA \n 1: Responsable Inscripto \n 2: Exenta"
     );
